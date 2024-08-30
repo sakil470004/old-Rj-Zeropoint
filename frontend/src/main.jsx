7c598dad3f7d56b5d856130dd6589c3678ed382b
Mynul Islam <mynul.sakil@gmail.com>
2024-08-31 01:23:29 +0600

    add route separate ly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+import "./index.css";
+import {  RouterProvider } from "react-router-dom";
+import { router } from "./router/router.jsx";
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
